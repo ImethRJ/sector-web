02fa605d4bb85c1ec4628f155459991de01c1b5f
Imeth <136222731+ImethRJ@users.noreply.github.com>
2026-01-22 19:31:24 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-// src/App.jsx
-import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react'; // Import useEffect
+import { Routes, Route, useLocation } from 'react-router-dom'; // Import useLocation
+import ReactGA from "react-ga4"; // Import React GA4
 import SectorHome from './components/SectorHome'
 import About from './components/About'
 import Footer from './components/Footer'
@@ -7,7 +8,10 @@ import TeachersHome from './components/TeachersHome'
 import TimetablePage from './components/TimetablePage'
 import NoticesPage from './components/NoticesPage'
 import AdminPage from './components/AdminPage'
-import AllTutors from './components/AllTutors' // 1. Import the new component
+import AllTutors from './components/AllTutors'
+
+// Initialize with your Measurement ID from the screenshot
+ReactGA.initialize("G-ETQXPG2C08");
 
 const LandingPage = () => (
   <div className="flex flex-col min-h-screen">
@@ -21,21 +25,28 @@ const LandingPage = () => (
 );
 
 function App() {
+  const location = useLocation();
+
+  // This effect runs every time the route (URL) changes
+  useEffect(() => {
+    ReactGA.send({
+      hitType: "pageview",
+      page: location.pathname + location.search
+    });
+  }, [location]);
+
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-
-      {/* 2. Add the route for the All Tutors page */}
       <Route path="/all-tutors" element={
         <>
           <AllTutors />
           <Footer />
         </>
       } />
-
       <Route path="/sector19365" element={<AdminPage />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
