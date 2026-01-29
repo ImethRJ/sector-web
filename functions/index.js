0aff305e95cdcfc6aa1f4b548898400690c54079
ImethRJ <136222731+ImethRJ@users.noreply.github.com>
2026-01-29 11:41:15 +0530

    update

--- functions/index.js
+++ functions/index.js
@@ -9,18 +9,15 @@ setGlobalOptions({ maxInstances: 10 });
 const app = express();
 
 // 1. Prerender.io Middleware Integration
-// This intercepts crawler requests (Bing, Google, etc.) and routes them to Prerender.io
 app.use(require('prerender-node')
-    .set('prerenderToken', 'xV3xxGRXT1mbc3fTwloG') // <-- Replace with your real token from Prerender.io
+    .set('prerenderToken', 'xV3xxGRXT1nbc3fTwloG') 
 );
 
 // 2. Serve the Static React App
-// This tells the function where to find your 'index.html' file to serve it to normal users
 app.get("*", (req, res) => {
-    // Note: Adjust the path below to point to where your build files are (usually '../public/index.html')
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    // CORRECTED: Pointing to '../dist/index.html' to match your firebase.json
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 // 3. Export the Function as 'ssr'
-// You will reference this name in your firebase.json rewrites
 exports.ssr = onRequest(app);
