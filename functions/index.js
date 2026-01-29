687b45f12e7b156922108fceece000d912d89972
ImethRJ <136222731+ImethRJ@users.noreply.github.com>
2026-01-29 13:10:49 +0530

    firebase up

--- functions/index.js
+++ functions/index.js
@@ -3,13 +3,12 @@ const { setGlobalOptions } = require("firebase-functions");
 const express = require("express");
 const path = require("path");
 
-// Configuration: Max 10 instances to manage costs
+// Configuration: Max 10 instances
 setGlobalOptions({ maxInstances: 10 });
 
 const app = express();
 
 // 1. Updated Security Policy (CSP)
-// Now allows Poppins fonts from your domain and Google Fonts
 app.use((req, res, next) => {
     res.setHeader(
         "Content-Security-Policy",
@@ -18,19 +17,25 @@ app.use((req, res, next) => {
     next();
 });
 
-// 2. Prerender.io Middleware
+// 2. Serve Static Assets (CSS, JS, Images)
+/** * This is critical. Since you moved 'dist' inside 'functions', 
+ * you must tell Express to serve the static files from this new path.
+ */
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// 3. Prerender.io Middleware
 app.use(require('prerender-node')
     .set('prerenderToken', 'xV3xxGRXT1nbc3fTwloG') 
 );
 
-// 3. Serve the Static React App
+// 4. Serve the Static React App (The entry point)
 app.get("*", (req, res) => {
     /**
-     * Path Resolution: 
-     * In the cloud, the 'dist' folder must be deployed alongside functions.
-     * We look for it in the parent directory relative to this script.
+     * CORRECTED PATH: 
+     * Now that 'dist' is INSIDE your functions folder, 
+     * we remove the '../' and point to './dist/index.html'.
      */
-    const indexPath = path.resolve(__dirname, '../dist/index.html');
+    const indexPath = path.join(__dirname, 'dist', 'index.html');
     
     res.sendFile(indexPath, (err) => {
         if (err) {
@@ -40,5 +45,5 @@ app.get("*", (req, res) => {
     });
 });
 
-// 4. Export the Function as 'ssr'
+// 5. Export the Function as 'ssr'
 exports.ssr = onRequest(app);
